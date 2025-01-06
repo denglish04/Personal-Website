eb59c42fc876c3bea665b82a375341df1e3b9aef
Djasahn English <147298260+denglish04@users.noreply.github.com>
2025-01-05 20:41:19 -0600

    Edit UI
    Set to Under Construction site, and added href to linked in

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
+
+      <h1>Djasahn English</h1>
+      <h3>This site is currently under construction. For more information visit my <a href="https://www.linkedin.com/in/djasahn-english">LinkedIn</a></h3>
+      
+      <div className="pageButtons">
+        <button>
+          About
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button>
+          Blog
+        </button>
+        <button>
+          Projects
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more about me about me. My name is Djasahn. 
-      </p>
     </>
   )
 }
