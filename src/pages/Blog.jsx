d52784c42325017d46ad13e50ddc0099f48a4b9d
Djasahn English <147298260+denglish04@users.noreply.github.com>
2025-01-08 10:51:42 -0600

    Update Blog.jsx
    included a text box and connected submit button for blog topic suggestions. submit button triggers alert

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,7 +1,10 @@
 import Header from '../components/Header'
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
 export default function Blog(){
 
+    const [text, setText] = useState('');
     const navigate = useNavigate();
 
     const handleClickHome = () => {
@@ -12,12 +15,24 @@ export default function Blog(){
         navigate('/about');  // Navigate to About page using the navigate hook
     };
 
+    const handleChange = (event) => {
+        setText(event.target.value);
+    }
+    const handleSubmit = () =>{
+        alert(`You've submitted the topic: ${text}`);
+        setText('');  // Clear the input field after submission
+    }
+
     return(
         <>
             <Header />
             <h2>Blog Page</h2>
             <button onClick={handleClickHome}> Home Page </button>
             <button onClick={handleClickAbout}> About Page </button>
+            <div>
+                <input type="text" value={text} onChange={handleChange} placeholder = "Blog Topic Suggestions or Requests..."/>
+                <button onClick={handleSubmit}> Submit </button>
+            </div>
         </>
     )
 }
