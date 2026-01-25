b3024af0ee36751060289d79e92b210f3d75df82
Djasahn English <denglish@oxy.edu>
2026-01-25 14:38:30 -0800

    New project component for the Oxy mobile shop

---
+++ src/pages/projects/MiniMobileShop.jsx
@@ -0,0 +1,22 @@
+import Header from '../../components/Header'
+import { useNavigate } from 'react-router-dom';
+
+export default function Projects(){
+    const navigate = useNavigate();
+
+    const handleClickHome = () => {
+        navigate('/');  // Navigate to home page using the navigate hook
+    };
+
+    const handleClickProjects = () => {
+	navigate("/projects")
+    }
+return(
+        <>
+            <Header />
+            <h2>2D Multiplayer</h2>
+	    <p> this is the 2D Multiplayer project, check it out this netlify hosted site:</p>
+	    <a href="https://blickxel.netlify.app/" target="_blank" rel="noopener noreferrer"> Click Here</a>
+	</>
+    )
+}
