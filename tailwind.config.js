19a3282a57f9ad8b4809077c8177b04d7ccf9760
Djasahn English <denglish@oxy.edu>
2026-01-25 17:32:16 -0800

    css changes for animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    screens: {
-	sm: '640px', //mobile
-	md: '768px', //tablet
-	lg: '1024', //laptop 
-	xl: '1280', //desktop
-    }
+    extend: {
+      keyframes: {
+        nameCycle: {
+          "0%, 100%": { color: "#f97316" }, // orange
+          "33%": { color: "#000000" },      // black
+          "66%": { color: "#ef4444" },      // red
+        },
+      },
+      animation: {
+        nameCycle: "nameCycle 3s infinite linear",
+      },
+      screens: {
+        sm: '640px', // mobile
+        md: '768px', // tablet
+        lg: '1024px', // laptop
+        xl: '1280px', // desktop
+      },
+    },
   },
   plugins: [],
-}
+};
 
